b5af64cfc3e05b705b05050b3ea484dde40fa5bd
Arindam <107887918+Arindam905@users.noreply.github.com>
2023-07-04 18:52:59 +0530

    train.py

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import json
 from nltk_utils import tokenize, stem
+import numpy as np
 
 with open("intents.json", 'r') as f:
     intents = json.load(f)
@@ -11,5 +12,25 @@ tags = []
 for intent in intents['intents']:
     tag = intent['tags']
     tags.append(tag)
-    for pattern in intents['pattern']:
-        w =
+    for pattern in intent['pattern']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+        
+ignore_words = ['?', '!', '.' , ',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+X_train = []
+Y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    
+    label = tags.index(tag)
+    Y_train.append(label)
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)    
+    
