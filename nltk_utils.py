fb9064a53b6daa9ea38fc502f17c46e540ae7c2b
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2023-07-04 19:01:56 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,7 @@
 import nltk
-#nltk.download('punkt')
+#nltk.download('punkt')  #This command should be runned once to install the 'punkt' module of nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 stemmer = PorterStemmer()
 
 
@@ -9,10 +10,16 @@ def tokenizer(word):
 
 
 def stem(word):
-    return stemmer.stem(word)
+    return stemmer.stem(word.lower())
 
 
-def bag_of_words(word):
-    pass
+def bag_of_words(tokenized_sentence, words):
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
 
 
